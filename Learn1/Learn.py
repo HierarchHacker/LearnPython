b1893edeaf99685623ddba7c64f69781dd1c78c4
Hierarch <1762572816@qq.com>
2020-10-01 21:47:10 +0800

    分支结构

--- Learn1/Learn.py
+++ Learn1/Learn.py
@@ -8,7 +8,7 @@ else:
         y = 3 * 23 + x
 print(f'f({x:.4f}) = {y:.5f}')
 '''以上为嵌套结构
-一下为扁平化结构'''
+以下为扁平化结构'''
 score = float(input('Please input your score:'))
 if score >= 90:
     grade = 'A'
@@ -21,3 +21,15 @@ elif score >= 60:
 else:
     grade = 'E'
 print('grade : ', grade)
+# 三角形的周长和面积
+a = float(input('a = '))
+b = float(input('b = '))
+c = float(input('c = '))
+if a + b > c and a + c > b and b + c > a:
+    C = a + b + c
+    half = C / 2
+    area = (half * (half - a) * (half - b) * (half - c)) ** 0.5
+    print(f'C = {C:.2f}')
+    print(f'area = {area:.3f}')
+else:
+    print('NO')
