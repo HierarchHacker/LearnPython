a11571a98bd8fe9820bd7b0e1a15635ba9fe936d
Hierarch <1762572816@qq.com>
2020-10-02 11:46:15 +0800

    猜数字小游戏，while循环

--- Learn1/Learn.py
+++ Learn1/Learn.py
@@ -1,16 +1,21 @@
-import random   # 此为导入random模块，导入后需要空一行再写代码
-# 此为空出来的一行
-print(random.random())    # 随机生成一个0~1的浮点数
-print(random.uniform(3, 4))    # 随机生成3~4的浮点数
-print(random.randint(0, 100))    # 随机生成0~100的整数
-print(random.randrange(1, 100, 2))   # 随机生成1~100的奇数，2为步长
-print(random.choice(['as', 2, 5, 7]))    # 随机选择 as，2，5，7中的任一字符，
-print(random.choice('dksajfla'))    # 随机生成dksajfla中的任一字符
-x = list(range(1, 10))  # 生成一个1，10区间的整数列表，前闭后开
-print(x)
-random.shuffle(x)   # 此为洗牌，即打乱列表中的顺序
-print(x)
-y = list(range(2, 14))
-print(y)
-s = random.sample(x, 4)  # 样品抽取，从指定序列中随机获取指定长度的片段。4，为获取的长度
-print(s)
+import random
+
+answer = random.randint(1, 11)
+counter = 0
+while True:  # 此步while true并不是“如果正确”，而是构造了一个条件恒成立的循环，也就意味着如果不做特殊处理，循环不会结束
+    counter += 1
+    number = int(input('请输入：'))
+    if number < answer:
+        print('Bigger')
+    elif number > answer:
+        print('Smaller')
+    else:
+        print('Congratulation!')
+        break
+print(f'Counter = {counter}')
+'''以上为break的用法，作用是提前结束循环
+另一个关键字是continue，可以i放弃本次循环后续的代码直接让循环进入下一轮'''
+for i in range(1,10):
+    for j in range(1,i + 1):
+        print(f'{i} * {j} = {i * j}', end='\t')
+    print()
