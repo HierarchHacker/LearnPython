c8c602656462f079808776a1ad9b8d048c12a4f2
Hierarch <1762572816@qq.com>
2020-10-04 16:49:11 +0800

    猜数字小游戏重制版

--- Learn1/Learn.py
+++ Learn1/Learn.py
@@ -1,15 +1,15 @@
-total = 0
-for x in range(1, 101):
-    total += x
-print(total)
-# 以上为1~100的和
-ou = 0
-for y in range(2, 101, 2):
-    ou += y
-print(ou)
-# 以上为1~100之间偶数的和
-ji = 0
-for z in range(1, 100, 2):
-    ji += z
-print(ji)
-# 以上为1~100奇数的和
+import random
+
+answer = random.randint(1, 11)
+counter = 0
+while True:
+    player = int(input('Please input your answer:'))
+    counter += 1
+    if player < answer:
+        print('Bigger!')
+    elif player > answer:
+        print('Smaller!')
+    else:
+        print('Congratulations!')
+        break
+print(f'Counter = {counter}')
