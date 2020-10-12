09e03be5fcfc4978ab2cf379f7b1df4413c301f8
Hierarch Thurs <1762572816@qq.com>
2020-10-12 22:13:45 +0800

    Hanshu

--- Learn1/Others.py
+++ Learn1/Others.py
@@ -30,14 +30,65 @@
 # print(fm // fn // fm_n)
 
 # 构造函数
-def f(num):
-    result = 1
-    for x in range(1, num + 1):
-        result *= x
-        return result
-# 函数和下面的代码应该空两行
+# def f(num):
+#     result = 1
+#     for x in range(1, num + 1):
+#         result *= x
+#         return result
+# # 函数和下面的代码应该空两行
+#
+#
+# m = int(input('m = '))
+# n = int(input('n = '))
+# print(f(m) // f(n) // f(m - n))
 
 
-m = int(input('m = '))
-n = int(input('n = '))
-print(f(m) // f(n) // f(m - n))
+# m = int(input('m = '))
+# n = int(input('n = '))
+# fm = 1
+# for num in range(1, m + 1):
+#     fm *= num
+# fn = 1
+# for num in range(1, n + 1):
+#     fn *= num
+# fm_n = 1
+# for num in range(1, m - n + 1):
+#     fm_n *= num
+# print(fm // fn // fm_n)
+
+# def f(num):
+#     result = 1
+#     for x in range(1, num + 1):
+#         result *= x
+#     return result
+#
+#
+# m = int(input('m = '))
+# n = int(input('n = '))
+# print(f(m) // f(n) // f(m - n))
+
+
+# def add(*args):
+#     total = 0
+#     for val in args:
+#         total += val
+#     return total
+#
+#
+# print(add())
+# print(add(1))
+# print(add(1, 2))
+# print(add(1, 2, 3))
+# print(add(1, 2, 4, 5, 6, 6, 4, 3))
+
+
+import module
+import module2
+
+
+module.foo()
+module2.foo()
+
+
+abs(-1)
+print(abs(-1))
